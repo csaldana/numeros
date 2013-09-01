b3ec3e1d9983f801d4875bc0a29e5461a368a820
csaldana <jonathan.csaldana@alumno.udg.mx>
2013-09-01 18:21:28 -0500

    codigo inicial

---
+++ numerosaleatorios.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+	int main(){
+		int r; // RAIZ
+		int a; // CONSTANTE MULTIPLICATIVA
+		int c; // CONSTANTE ADITIVA
+		int m; // MODULO
+        int i;
+        printf("Raiz:\t");
+        scanf("%d",&r);
+        printf("\nConstante multiplicativa:\t");
+        scanf("%d",&a);
+        printf("\nConstante aditiva:\t");
+        scanf("%d",&c);
+        printf("\nModulo:\t");
+        scanf("%d",&m);
+		printf(" r = ( a * r +c)mod m ");
+
+		for( i=0; i<=50; i++){
+
+			r = (a*r+c)%m;
+			printf(" %i - ( %i * %i + %i )mod %i \n", i, a, r, c, m);
+		}
+
+
+
+		return 0;
+	}
+
